32412812dc7a1cc1af13f466f7260f14949a79bf
Alberto Rodrigues <albertor@ime.usp.br>
2025-04-27 16:20:52 -0300

    add introduction langgraph e continuação da geração de um token pelo GPT

---
+++ gpt/vocab.py
@@ -0,0 +1,53 @@
+tokens = [
+    # Pronomes
+    "eu", "você", "ele", "ela", "nós", "vocês", "eles", "elas", "me", "te",
+    "se", "nos", "vos", "lhe", "lhes", "mim", "ti", "si",
+
+    # Verbos comuns
+    "ser", "estar", "ter", "haver", "fazer", "poder", "dizer", "ver", "dar", "saber",
+    "querer", "chegar", "passar", "dever", "ficar", "deixar", "pensar", "vir", "conhecer",
+
+    # Substantivos
+    "casa", "tempo", "dia", "mundo", "homem", "mulher", "vida", "mão", "olho", "palavra",
+    "caminho", "gato", "cachorro", "carro", "livro", "porta", "rua", "trabalho", "dinheiro", "noite",
+
+    # Adjetivos
+    "bom", "mau", "feliz", "triste", "grande", "pequeno", "novo", "velho", "forte", "fraco",
+    "belo", "feio", "inteligente", "rápido", "lento",
+
+    # Preposições / Conectivos
+    "em", "de", "com", "por", "para", "sobre", "entre", "até", "após", "antes",
+    "sem", "sob", "contra", "durante", "perante", "trás",
+
+    # Artigos / Partículas
+    "o", "a", "os", "as", "um", "uma", "uns", "umas", "do", "da",
+    "dos", "das", "este", "esta", "estes", "estas",
+
+    # Numerais
+    "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez",
+    "vinte", "trinta", "cem", "mil", "milhão",
+
+    # Expressões comuns
+    "sim", "não", "talvez", "hoje", "amanhã", "ontem", "agora", "sempre", "nunca", "já",
+    "ainda", "depois", "antes", "aqui", "ali", "lá", "longe", "perto",
+
+    # Lugares / Coisas
+    "escola", "cidade", "país", "terra", "céu", "mar", "praia", "montanha", "rio", "floresta",
+    "livraria", "mercado",
+
+    # Outros substantivos úteis
+    "amigo", "amiga", "criança", "professor", "aluno", "trabalho", "pessoa", "família", "história", "amor",
+
+    # Verbos auxiliares
+    "vou", "vai", "vamos", "vão", "fui", "foi", "foram",
+    "estou", "está", "estamos", "estão",
+
+    # Outros verbos
+    "gostar", "amar", "odiar", "correr", "andar", "viajar", "sorrir", "chorar", "brincar", "estudar",
+
+    # Comuns adicionais
+    "beber", "comer", "dormir", "acordar", "abrir", "fechar", "comprar", "vender", "ligar", "desligar",
+
+    # Conectores e partículas
+    "e", "mas", "ou", "porque", "que", "como", "quando", "onde", "quem", "qual"
+]
